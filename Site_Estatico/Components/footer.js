8809445624bc08becf3b4d70113cc969c73d7331
MAGES.0451 <lucas.ribeiro.cruz12@gmail.com>
2023-07-09 18:04:52 -0300

    mod footer

--- Site_Estatico/Components/footer.js
+++ Site_Estatico/Components/footer.js
@@ -13,7 +13,7 @@ class footer extends HTMLElement {
        
         <div class="row my-4">
           
-          <div class="col-lg-8 col-md-6 mb-4 mb-md-0">
+          <div class="col-lg-4 col-md-6 mb-4 mb-md-0">
   
             
               <img src="images/LOGO BVM CONTABILIDADE(logo).png" height="70" alt=""
@@ -24,6 +24,28 @@ class footer extends HTMLElement {
   
             
           </div>
+
+          <div class="col-lg-4 col-md-6 mb-4 mb-md-0">
+            <h4 class="text-uppercase mb-4">Links</h4>
+            <div class="footer-link">
+            <ul class="list-unstyled">
+          
+                <li>
+                  <p><i class="fas fa-map-marker-alt pe-2"></i><a href="index.html">Home</a></p>
+                </li>
+                <li>
+                  <p><i class="fas fa-phone pe-2"></i><a href="servicos.html">Serviços</a></p>
+                </li>
+                <li>
+                  <p><i class="fas fa-envelope pe-2 mb-0"></i><a href="QuemSomos.html">Quem somos</a></p>
+                </li>
+                <li>
+                  <p><i class="fas fa-envelope pe-2 mb-0"></i><a href="Contato.html">Contato</a></p>
+                </li>
+              </ul>
+              </div>
+          </div>
+     
 
           <div class="col-lg-4 col-md-6 mb-4 mb-md-0">
             <h4 class="text-uppercase mb-4">Contato</h4>

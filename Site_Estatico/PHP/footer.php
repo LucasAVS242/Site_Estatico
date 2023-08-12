57c52be7c661897385042887344f192304c2f6d3
MAGES.0451 <lucas.ribeiro.cruz12@gmail.com>
2023-08-12 17:50:17 -0300

    php

---
+++ Site_Estatico/PHP/footer.php
@@ -0,0 +1,5 @@
+<hr> <!--linha -->
+
+<div class="container"> &copy;2019 </div> <!--mostra o simbolo de copyright e 2019 no final da tela -->
+
+</body> </html>
---
+++ Site_Estatico/PHP/index.php
@@ -0,0 +1,17 @@
+<?php
+
+
+require_once'header.php';
+?>
+
+<div class="container">
+    <div class="jumbotron">
+        <h1>Basic CRUD in PHP</h1>
+
+    </div>
+</div>
+
+<?php
+require_once'footer.php';
+
+

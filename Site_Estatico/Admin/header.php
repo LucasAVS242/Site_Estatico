beb15b65322e89c4a260255b2176f4fe16d550e3
LucasAVS242 <lucasventurini242@outlook.com>
2023-08-16 22:07:16 -0300

    mod PHP

--- Site_Estatico/Admin/header.php
+++ Site_Estatico/Admin/header.php
@@ -6,12 +6,9 @@
   <title>BVM Auditores</title>
   <link rel="icon" type="image/x-icon" href="../images/favicon.ico">
   <!-- Link CSS Bootstrap -->
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet"
-    integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
   <!-- Link JS Bootstrap -->
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"
-    integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz"
-    crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
 
   <link rel="stylesheet" href="../styles/main.css" type="text/css">
 
@@ -21,7 +18,7 @@
 
   <style>
     .container img {
-        background-color: #eeeeee;
+      background-color: #eeeeee;
     }
   </style>
 </head>
@@ -34,8 +31,7 @@
         <a class="navbar-brand" href="index.php">
           <img class="logo" src="../images/logotipo.png" alt="BVM Contabilidade">
         </a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
-          aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarNav">
@@ -43,8 +39,13 @@
             <li class="nav-item">
               <a class="nav-link" href="index.php">Home</a>
             </li>
-            <li class="nav-item">
-              <a class="nav-link" href="insert.php">Adicionar</a>
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" role="button" data-bs-toggle="dropdown" aria-expanded="false">Cadastrar</a>
+              <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                <li><a class="dropdown-item" href="insert.php">Usuário</a></li>
+                <li><a class="dropdown-item" href="../index.html">Cliente</a></li>
+                <li><a class="dropdown-item" href="../index.html">Serviços</a></li>
+              </ul>
             </li>
             <li class="nav-item">
               <a class="nav-link" href="users.php">Usuários</a>
@@ -52,8 +53,7 @@
           </ul>
           <ul class="navbar-nav ms-auto">
             <li class="nav-item dropdown">
-              <a class="nav-link dropdown-toggle" role="button" data-bs-toggle="dropdown"
-                aria-expanded="false">Conta</a>
+              <a class="nav-link dropdown-toggle" role="button" data-bs-toggle="dropdown" aria-expanded="false">Conta</a>
               <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
                 <li><a class="dropdown-item" href="../index.html">Sair</a></li>
               </ul>

aaf84b6bd42470350240861a6e2a872ed5cec59e
MAGES.0451 <lucas.ribeiro.cruz12@gmail.com>
2023-08-21 19:52:38 -0300

    f

--- Site_Estatico/Admin/footer.php
+++ Site_Estatico/Admin/footer.php
@@ -1,5 +1,9 @@
-<hr> <!--linha -->
 
-<div class="container"> &copy;2023 </div>
+
+<footer style="position:relative">
+<div class="text-center p-3" style="background-color: #247454; color:#fff;">
+        © 2023 Copyright: BVM Contabilidade - Todos os Direitos Reservados
+      </div>
+</footer>
 
 </body> </html>

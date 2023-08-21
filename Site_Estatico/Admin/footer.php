dde2ea4803bedf22652755b098540e9ecb53aff4
MAGES.0451 <lucas.ribeiro.cruz12@gmail.com>
2023-08-21 20:07:09 -0300

    footer

--- Site_Estatico/Admin/footer.php
+++ Site_Estatico/Admin/footer.php
@@ -1,9 +1,70 @@
 
 
-<footer style="position:relative;margin-top:4%">
-<div class="text-center p-3" style="background-color: #247454; color:#fff;">
+<p style="padding: 10px;"></p>
+    <footer style="background-color: #3cab7b;" class="text-center text-lg-start text-white">
+   
+      <div class="container p-4">
+       
+        <div class="row my-4">
+          
+          <div class="col-lg-4 col-md-6 mb-4 mb-md-0">
+  
+            
+              <img src="../images/LOGO BVM CONTABILIDADE(logo).png" height="70" alt=""
+                   loading="lazy" />
+            
+  
+            <p style="padding-left:10px;" class="text-left">Auditores Contabilidade</p>
+  
+            
+          </div>
+
+          <div class="col-lg-4 col-md-6 mb-4 mb-md-0">
+            <h4 class="text-uppercase mb-4">Cadastrar</h4>
+            <div class="footer-link">
+            <ul class="list-unstyled">
+          
+                <li>
+                  <p><a href="cadastrar-usuario.php">Usuario</a></p>
+                </li>
+                <li>
+                  <p><a href="cadastrar-cliente.php">Cliente</a></p>
+                </li>
+                <li>
+                  <p><a href="cadastrar-servico.php">Serviço</a></p>
+                </li>
+                
+              </ul>
+              </div>
+          </div>
+     
+
+          <div class="col-lg-4 col-md-6 mb-4 mb-md-0">
+            <h4 class="text-uppercase mb-4">Resgistros</h4>
+  
+            <ul class="list-unstyled footer-link">
+            <li>
+                  <p><a href="usuarios.php">Usuario</a></p>
+                </li>
+                <li>
+                  <p><a href="clientes.php">Cliente</a></p>
+                </li>
+                <li>
+                  <p><a href="servicos.php">Serviço</a></p>
+                </li>
+              </ul>
+          </div>
+     
+        </div>
+       
+      </div>
+      
+  
+      
+      <div class="text-center p-3" style="background-color: #247454">
         © 2023 Copyright: BVM Contabilidade - Todos os Direitos Reservados
       </div>
-</footer>
+      
+    </footer>
 
 </body> </html>

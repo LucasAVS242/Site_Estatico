b7e29bd6f221b304e8bb7410d46eb538e3ec25b7
LucasAVS242 <lucasventurini242@outlook.com>
2023-08-21 22:26:57 -0300

    add fontawesome local

--- Site_Estatico/Admin/header.php
+++ Site_Estatico/Admin/header.php
@@ -6,7 +6,7 @@
   <title>BVM Auditores</title>
   <link rel="icon" type="image/x-icon" href="../images/favicon.ico">
   <!-- Link Font Awesome  -->
-  <script src="https://kit.fontawesome.com/26c14cdb57.js" crossorigin="anonymous"></script>
+  <link href="../styles/fontawesome/css/all.css" rel="stylesheet"></link>
   <!-- Link CSS Bootstrap -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
   <!-- Link JS Bootstrap -->
--- Site_Estatico/autenticar.php
+++ Site_Estatico/autenticar.php
@@ -10,7 +10,7 @@ session_start();
     <meta charset="UTF-8">
     <title>Login</title>
     <link rel="icon" type="image/x-icon" href="images/favicon.ico">
-    <script src="https://kit.fontawesome.com/26c14cdb57.js" crossorigin="anonymous"></script>
+    <link href="styles/fontawesome/css/all.css" rel="stylesheet"></link>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" 
       integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
     <link rel="stylesheet" href="styles/main.css" type="text/css">

2c6fffcc32067181f69aea61a88555256b9830e5
LucasAVS242 <lucasventurini242@outlook.com>
2023-08-26 17:57:39 -0300

    .

--- Site_Estatico/autenticar.php
+++ Site_Estatico/autenticar.php
@@ -8,7 +8,8 @@ session_start();
 
 <head>
     <meta charset="UTF-8">
-    <title>Login</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Autenticando...</title>
     <link rel="icon" type="image/x-icon" href="images/favicon.ico">
     <link href="styles/fontawesome/css/all.css" rel="stylesheet"></link>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" 

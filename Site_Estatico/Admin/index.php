527861d696fbd7ef33547095313040b0d58dcbe5
MAGES.0451 <lucas.ribeiro.cruz12@gmail.com>
2023-08-26 16:03:07 -0300

    a

--- Site_Estatico/Admin/index.php
+++ Site_Estatico/Admin/index.php
@@ -7,8 +7,9 @@ require_once 'header.php';
 <main>
     <p style="padding-left: 1%;"><i style="color: #46d160;" class="fa-solid fa-circle fa-2xs"></i>&nbsp;Usuário logado: <?php echo $_SESSION['usuario']; ?></p>
     <div class="container">
-
-        <img style="margin-top: 5%; background:none;" class="rounded" src="../images/LOGO BVM CONTABILIDADE(logo 2).png" width="1140" alt="">
+        <div class="row">
+        <img style="margin-top: 5%; background:none;" class="rounded" 
+        src="../images/LOGO BVM CONTABILIDADE(logo 2).png" width="720" alt="">
 
     </div>
     </div>

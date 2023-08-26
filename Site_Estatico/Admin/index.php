8d20ea5d0f8fdd910c9dbd3802c6f3266ee02ad9
LucasAVS242 <lucasventurini242@outlook.com>
2023-08-26 17:39:58 -0300

    .

--- Site_Estatico/Admin/index.php
+++ Site_Estatico/Admin/index.php
@@ -8,7 +8,7 @@ require_once 'header.php';
     <p style="padding-left: 1%;"><i style="color: #46d160;" class="fa-solid fa-circle fa-2xs"></i>&nbsp;Usuário logado: <?php echo $_SESSION['usuario']; ?></p>
     <div class="container">
 
-        <img style="margin-top: 5%; background:none;" class="rounded" src="../images/LOGO BVM CONTABILIDADE(logo 2).png" width="1140" alt="">
+        <img style="margin-top: 5%; background:none; max-width:100%;height:auto;" class="rounded" src="../images/LOGO BVM CONTABILIDADE(logo 2).png" width="1140" alt="">
 
     </div>
     </div>

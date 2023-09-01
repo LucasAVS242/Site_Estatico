664a8a13caf0a291b6bb1dcf38204f2a07598bc3
LucasAVS242 <lucasventurini242@outlook.com>
2023-09-01 18:45:27 -0300

    centralized img index.php

--- Site_Estatico/Admin/index.php
+++ Site_Estatico/Admin/index.php
@@ -7,10 +7,9 @@ require_once 'header.php';
 <main>
     <p style="padding-left: 1%;"><i style="color: #46d160;" class="fa-solid fa-circle fa-2xs"></i>&nbsp;Usuário logado: <?php echo $_SESSION['usuario']; ?></p>
     <div class="container">
-
-        <img style="margin-top: 5%; background:none; max-width:100%;height:auto;" class="rounded" src="../images/LOGO BVM CONTABILIDADE(logo 2).png" width="1140" alt="">
-
-    </div>
+        <div style=" display: flex; justify-content: center;">
+            <img style="margin-top: 5%; background:none; max-width:100%;height:auto;" class="rounded" src="../images/LOGO BVM CONTABILIDADE(logo 2).png" width="1140" alt="">
+        </div>
     </div>
 </main>
 
